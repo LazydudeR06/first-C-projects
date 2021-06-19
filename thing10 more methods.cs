9662174e7e113f4216fdae42bfda66f31908d4c3
LazydudeR06 <84030130+LazydudeR06@users.noreply.github.com>
2021-06-19 16:39:19 +0530

    Add files via upload

---
+++ thing10 more methods.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp_Shell
+{
+
+    public class Program 
+    {
+        public static void Main()
+        {
+			
+        }
+        int chat1(){
+        	
+        }
+    }
+}
